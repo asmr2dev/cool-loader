e8793153f6a51d317a910b5a42b11a82382ca380
asmr2dev <asmr2dev@gmail.com>
2023-03-31 19:10:13 +0430

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const contentsText = [
+    "loading",
+    "ASMR2DEV"
+]
+
+const body = document.querySelector("body");
+
+const loaders = contentsText.map(text => {
+    const letters = text.split("");
+    const loader = letters.map(letter => {
+        return `<span>${letter}</span>`
+    })
+
+    return `<div class="loader">${loader.join("")}</div>`
+})
+
+const contents = loaders.join("");
+body.innerHTML = contents;
